79ca32e2ced2f87a5be765e0f6c3c8725dae48cb
mander207 <Minas_Anderiasian@cable.comcast.com>
2018-05-31 08:15:28 -0700

    Add some comments, doing a few NULL checks.

--- src/queue.c
+++ src/queue.c
@@ -20,9 +20,15 @@
 #include "queue_internal.h"
 #include "queue.h"
 #include "rebar-xxd.h"
-
+/*
+ queue.h implements a singly linked list FIFO
+ */
 
 /*
+ * Allocates memory for the queue, queue must be deleted by
+ * calling rebar_queue_delete(), all client data must have been 
+ * freed by the user of this API, Queue will only free it's own structures.
+ * Will assert in os if malloc() fails!
  */
 queue_t *rebar_queue_init (void)
 {
@@ -56,13 +62,14 @@ int rebar_queue_delete (queue_t *q)
 }
 
 /*
+ * returns and removes top most (head or oldest item) on the Queue.
  */
 void *rebar_queue_pop (queue_t *q)
 {
     void *data = NULL;
     queue_element_t *e;
 
-    if (q->head == NULL) {
+    if (q == NULL || q->head == NULL) {
         return data;
     }
 
@@ -79,10 +86,17 @@ void *rebar_queue_pop (queue_t *q)
 }
 
 /*
+ * pushes the item to the end (tail) of the queue,
  */
 int rebar_queue_push (void *data, queue_t *q)
 {
-    queue_element_t *e = (queue_element_t *) malloc(sizeof(queue_element_t));
+    queue_element_t *e;
+
+    if (NULL == q) {
+        return -1;
+    }
+
+    e = (queue_element_t *) malloc(sizeof(queue_element_t));
     e->data = data;
     if (0 == q->current_size) { // Empty queue
         q->tail = e;
@@ -98,6 +112,7 @@ int rebar_queue_push (void *data, queue_t *q)
 }
 
 /*
+ * returns "data" for the head, the queue is not affected by this call.
  */
 void *rebar_queue_peek (queue_t *q)
 {
@@ -110,24 +125,41 @@ void *rebar_queue_peek (queue_t *q)
 }
 
 /*
+ * returns number of items in the Queue
  */
 size_t rebar_queue_size (queue_t *q)
 {
-    return q->current_size;
+    if (q) {
+        return q->current_size;
+    } else {
+        return 0;
+    }
 }
 
 /*
  */
 bool rebar_queue_is_empty(queue_t *q)
 {
-    return (0 == q->current_size);
+    if (q) {
+        return (0 == q->current_size);
+    } else {
+        return true;
+    }
 }
 
 /*
+ * prints length number of bytes (xxd) per link in the Queue
+ * "data" is opaque to the queue.
  */
 void rebar_queue_print(queue_t *q, const size_t length)
 {
-    queue_element_t *e = q->head;
+    queue_element_t *e;
+
+    if (NULL == q) {
+        return;
+    }
+
+    e = q->head;
 
     if (q->current_size) {
         do {
